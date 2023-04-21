38b1cd4a45c5286e6496457656c29e638ee2645e
neeraj pathak <pathakn@mediamarktsaturn.com>
2023-04-21 12:55:15 +0200

    e

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,13 +3,13 @@ import { message, danger } from 'danger';
 const modifiedMD = danger.git.modified_files.join('- ');
 message(`<<<<====>>>>Changed Files in t
 his PR: \n - ${modifiedMD}`);
-console.log('danger--actions---> 1', danger.github.api.actions);
-console.log('danger--actions---> 2', danger.github.api.actions.getJobForWorkflowRun('hello'));
-console.log('danger--actions---> 3', danger.github.api.actions.getWorkflow());
-console.log('danger--actions---> 4', danger.github.api.actions.getWorkflowRunUsage());
-console.log('danger--actions---> 5', danger.github.api.actions.getWorkflowUsage());
-console.log('danger--actions---> 6', danger.github.api.actions.listJobsForWorkflowRun());
-console.log('danger--actions---> 6', danger.github.api.actions.listWorkflowRuns());
+// console.log('danger--actions---> 1', danger.github.api.actions);
+// console.log('danger--actions---> 2', danger.github.api.actions.getJobForWorkflowRun('hello'));
+// console.log('danger--actions---> 3', danger.github.api.actions.getWorkflow());
+// console.log('danger--actions---> 4', danger.github.api.actions.getWorkflowRunUsage());
+// console.log('danger--actions---> 5', danger.github.api.actions.getWorkflowUsage());
+// console.log('danger--actions---> 6', danger.github.api.actions.listJobsForWorkflowRun());
+// console.log('danger--actions---> 6', danger.github.api.actions.listWorkflowRuns());
 
 console.log('danger--billing---> 1', danger.github.api.billing.getGithubActionsBillingOrg());
 console.log('danger--billing---> 2', danger.github.api.billing.getGithubActionsBillingUser());
