b2de8f0bbb4bf4f960ba16465a190db0f56b7774
neeraj pathak <pathakn@mediamarktsaturn.com>
2023-04-21 08:53:51 +0200

    2

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,8 @@
 import { message, danger } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join('- ');
-message(`<<<<====>>>>Changed Files in this PR: \n - ${modifiedMD}`);
+message(`<<<<====>>>>Changed Files in t
+his PR: \n - ${modifiedMD}`);
+console.log('danger', danger);
+
 message(`<<<<====>>>>Changed Files in this PR: ===>>>>>>>`);
