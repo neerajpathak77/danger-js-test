9fe8da49c0134d44a517726dbd4302dcbe739375
neeraj pathak <pathakn@mediamarktsaturn.com>
2023-04-30 15:59:40 +0200

    dededede

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,46 +3,27 @@ import { message, danger } from 'danger';
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message(`<<<<====>>>>Changed Files in this PR: \n - ${modifiedMD}`);
-// console.log('danger--actions---> 1', danger.github.api.actions);
-// console.log('danger--actions---> 2', danger.github.api.actions.getJobForWorkflowRun('hello'));
-// console.log('danger--actions---> 3', danger.github.api.actions.getWorkflow());
-// console.log('danger--actions---> 4', danger.github.api.actions.getWorkflowRunUsage());
-// console.log('danger--actions---> 5', danger.github.api.actions.getWorkflowUsage());
-// console.log('danger--actions---> 6', danger.github.api.actions.listJobsForWorkflowRun());
-// eslint-disable-next-line promise/catch-or-return
 
-// console.log('danger-gvar---> 1', danger.github.thisPR.owner)
-// console.log('danger-gvar---> 2', danger.github.thisPR.repo)
-// console.log('danger-gvar---> 3', danger.github.pr.user)
-
-// const args = process.argv;
-// console.log('====>>>', args);
-
-// danger.github.api.actions
-//   .getActionsCacheList({
-//     owner: 'neerajpathak77',
-//     repo: 'danger-js-test'
-//   })
-//   .then(d => message(`9999999999999999 \n - ${JSON.stringify(d, null, 3)}`));
-
-danger.github.api.actions
-  .listRepoWorkflows({
+const test = async () => {
+  const workflows = await danger.github.api.actions.listRepoWorkflows({
     owner: 'neerajpathak77',
     repo: 'danger-js-test'
-  })
-  .then(d => {
-    console.log('danger----> 1333333', JSON.stringify(d, null, 3));
-
- 
-
-    danger.github.api.actions
-      .getJobForWorkflowRun({
-        owner: 'neerajpathak77',
-        repo: 'danger-js-test',
-        job_id: id
-      })
-      .then(d => message(`9999999999999999 \n - ${JSON.stringify(d, null, 3)}`));
   });
+
+  console.log('danger----> 11111111', JSON.stringify(workflows, null, 3));
+
+  const workflow = workflows.data.workflows.find(element => element.name == '.github/workflows/danger.yml') ?? {};
+
+  console.log('danger----> 222222222', JSON.stringify(workflow, null, 3));
+
+  danger.github.api.actions
+    .getJobForWorkflowRun({
+      owner: 'neerajpathak77',
+      repo: 'danger-js-test',
+      job_id: workflow.id
+    })
+    .then(d => message(`3333333 \n - ${JSON.stringify(d, null, 3)}`));
+};
 
 // console.log('danger--billing---> 1', danger.github.api.billing.getGithubActionsBillingOrg());
 // console.log('danger--billing---> 2', danger.github);
